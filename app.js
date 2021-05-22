b86b7498698df823da099b6da0b2f00f44c7ee36
munjalSaksham71 <sakshammunjal4@gmail.com>
2021-05-22 11:36:28 +0530

    bug fixed

--- app.js
+++ app.js
@@ -77,7 +77,8 @@ app.get("/contact", function(req, res){
   res.render("contact", {contactContent: contactContent});
 });
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("Server started on port 3000");
 });
